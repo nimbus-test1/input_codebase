eda26ef68e0ddf78c0100f92badeced3246bd331
nimbus-test1 <snipersport786@gmail.com>
2024-07-27 16:28:02 -0400

    Add Terraform configuration file

---
+++ Nimbus/tf_response.tf
@@ -0,0 +1,66 @@
+
+Terraform configuration content:
+```hcl
+# main.tf
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web_server" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "WebServerInstance"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "web_eip" {
+  instance = aws_instance.web_server.id
+}
+
+variable "ami" {
+  description = "AMI ID for the Ubuntu server"
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key name for the EC2 instance"
+  default     = "my-key"
+}
+```
