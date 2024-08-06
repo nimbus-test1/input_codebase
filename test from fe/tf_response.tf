404548e1e792063a684e87cdf40e3926b8db491e
nimbus-test1 <snipersport786@gmail.com>
2024-08-06 17:19:08 -0400

    Add Terraform configuration file

--- test from fe/tf_response.tf
+++ test from fe/tf_response.tf
@@ -4,12 +4,85 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_security_group" "lb" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "example-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb.id]
+  subnets            = [aws_subnet.subnet.id]
+
+  enable_deletion_protection = false
+}
+
+resource "aws_autoscaling_group" "example" {
+  desired_capacity     = 3
+  max_size             = 5
+  min_size             = 1
+  vpc_zone_identifier  = [aws_subnet.subnet.id]
+  health_check_type    = "EC2"
+  health_check_grace_period = 300
+
+  launch_configuration = aws_launch_configuration.example.id
+}
+
+resource "aws_launch_configuration" "example" {
+  name          = "example-lc"
+  image_id      = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
-  tags = {
-    Name = "example-instance"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.example.id
+  lb_target_group_arn    = aws_lb_target_group.example.arn
+}
+
+resource "aws_lb_target_group" "example" {
+  name     = "example-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
   }
 }
 ```
