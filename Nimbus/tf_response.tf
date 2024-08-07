163cdc23bf279b815c7d55d10ef19b02b25de605
nimbus-test1 <snipersport786@gmail.com>
2024-08-07 09:32:40 -0400

    Add Terraform configuration file

--- Nimbus/tf_response.tf
+++ Nimbus/tf_response.tf
@@ -1,44 +1,15 @@
 
 ```hcl
 provider "aws" {
-  region = var.region
+  region = "us-west-2"
 }
 
-resource "aws_db_instance" "mysql" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
-  name                 = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  parameter_group_name = "default.mysql8.0"
-  backup_window        = "03:00-04:00"
-  backup_retention_period = 7
-  skip_final_snapshot  = true
-}
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 
-variable "region" {
-  description = "AWS region to deploy the MySQL instance"
-  type        = string
-  default     = "us-west-2"
-}
-
-variable "db_name" {
-  description = "The name of the MySQL database"
-  type        = string
-  default     = "mydatabase"
-}
-
-variable "db_username" {
-  description = "The username for the MySQL database"
-  type        = string
-  default     = "admin"
-}
-
-variable "db_password" {
-  description = "The password for the MySQL database"
-  type        = string
-  default     = "password"
+  tags = {
+    Name = "example-instance"
+  }
 }
 ```
