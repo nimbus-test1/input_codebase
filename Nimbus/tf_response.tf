d13edee86f12a10ba86b53b4fbb0f02b2d990282
nimbus-test1 <snipersport786@gmail.com>
2024-08-07 09:31:11 -0400

    Add Terraform configuration file

--- Nimbus/tf_response.tf
+++ Nimbus/tf_response.tf
@@ -1,52 +1,44 @@
 
 ```hcl
 provider "aws" {
-  region = var.aws_region
+  region = var.region
 }
 
 resource "aws_db_instance" "mysql" {
   allocated_storage    = 20
-  storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0"
   instance_class       = "db.t2.micro"
   name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.mysql8.0"
+  backup_window        = "03:00-04:00"
+  backup_retention_period = 7
   skip_final_snapshot  = true
-
-  backup_window             = var.backup_window
-  backup_retention_period   = var.backup_retention_period
 }
 
-variable "aws_region" {
-  description = "The AWS region to deploy the MySQL instance in"
+variable "region" {
+  description = "AWS region to deploy the MySQL instance"
+  type        = string
   default     = "us-west-2"
 }
 
 variable "db_name" {
   description = "The name of the MySQL database"
-  default     = "mydb"
+  type        = string
+  default     = "mydatabase"
 }
 
 variable "db_username" {
   description = "The username for the MySQL database"
+  type        = string
   default     = "admin"
 }
 
 variable "db_password" {
   description = "The password for the MySQL database"
+  type        = string
   default     = "password"
-}
-
-variable "backup_window" {
-  description = "The daily time range during which automated backups are created if automated backups are enabled"
-  default     = "04:00-06:00"
-}
-
-variable "backup_retention_period" {
-  description = "The number of days to retain backups for"
-  default     = 7
 }
 ```
