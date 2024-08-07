b1ccf34d19411e53529d47644d5666dbc5b4d872
nimbus-test1 <snipersport786@gmail.com>
2024-08-07 09:33:12 -0400

    Add Terraform configuration file

--- Nimbus/tf_response.tf
+++ Nimbus/tf_response.tf
@@ -4,12 +4,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+}
 
-  tags = {
-    Name = "example-instance"
-  }
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
 }
 ```
